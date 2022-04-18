cfcf9efaf2e78c54edfff6a82caac4472c50b368
Ratishree <ratishreesingh@gmail.com>
2022-04-18 23:55:07 +0530

    Section 5.68

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 	const [filteredYear, setFilteredYear] = useState('2022');
@@ -12,25 +12,16 @@ const Expenses = (props) => {
 		console.log(selectedYear);
 	};
 
+	const filteredExpenses = props.expenses.filter(expense => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
+
+
 	return (
 		<Card className="expenses">
 			<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-			<ExpenseItem
-			title={props.expenses[0].title}
-			amount={props.expenses[0].amount}
-			date={props.expenses[0].date} />
-			<ExpenseItem
-			title={props.expenses[1].title}
-			amount={props.expenses[1].amount}
-			date={props.expenses[1].date} />
-			<ExpenseItem
-			title={props.expenses[2].title}
-			amount={props.expenses[2].amount}
-			date={props.expenses[2].date} />
-			<ExpenseItem
-			title={props.expenses[3].title}
-			amount={props.expenses[3].amount}
-			date={props.expenses[3].date} />
+			<ExpensesList expenses={filteredExpenses}/>
+
 		</Card>
 	);
 }
