60e460b125e9bf41ad3b2c24cba862c53e1b4493
Ratishree <ratishreesingh@gmail.com>
2022-04-18 15:45:44 +0530

    Section 4

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,9 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
+
 
 const ExpenseItem = (props) => {
 	return (
